9e73663dfd1ca32d3033494c5cb28b69d2f5f52f
kkacanja <your.email@example.com>
2025-01-16 16:02:01 -0500

    quality checks run

---
+++ gw200105/seob/28f/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+source /home/kkacanja/miniconda3/etc/profile.d/conda.sh
+conda activate /home/kkacanja/miniconda3/envs/SEOBNRv5E
+export LAL_DATA_PATH=/home/ksoni01/lalsuite/lalsuite-extra/data/lalsimulation/
+export OMP_NUM_THREADS=1
+
+pycbc_inference \
+--config-file /home/kkacanja/ecc_pe/gw200105/seob/28f/config.ini \
+--nprocesses 64 \
+--processing-scheme mkl \
+--output-file /home/kkacanja/ecc_pe/gw200105/seob/28f/result.hdf \
+--seed 190814 \
+--force \
+--verbose 
---
+++ gw200105/seob/plot.sh
@@ -0,0 +1,4 @@
+pycbc_inference_plot_posterior --input-file ./result.hdf.bkup \
+--output-file /home/kkacanja/public_html/ecc_pe/gw200105_posteriors_seob.png \
+--parameters inclination mchirp q spin1z spin2z eccentricity rel_anomaly \
+--z-arg snr --vmin 13.2 --vmax 13.8 \
