99d910718e39cc39e36622753b99c57fe4e3648d
kkacanja <your.email@example.com>
2025-01-17 10:18:17 -0500

    updated files

---
+++ gw190425/seob/plot.sh
@@ -0,0 +1,4 @@
+pycbc_inference_plot_posterior --input-file ./result.hdf \
+--output-file /home/kkacanja/ecc_pe/plots/gw190425/gw190425_posteriors_seob.png \
+--parameters inclination mchirp q spin1z spin2z eccentricity rel_anomaly \
+--z-arg snr --vmin 11.6 --vmax 12.2 \
