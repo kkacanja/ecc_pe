504e0b3398c7358bea36007301358fec71feb1cb
kkacanja <your.email@example.com>
2025-02-03 13:22:46 -0500

    runs with varying likelihood frequency

---
+++ gw200105/teob/vary_l1/22/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+source /home/kkacanja/miniconda3/etc/profile.d/conda.sh
+conda activate /home/kkacanja/miniconda3/envs/DALI
+export LAL_DATA_PATH=/home/ksoni01/lalsuite/lalsuite-extra/data/lalsimulation/
+export OMP_NUM_THREADS=1
+
+pycbc_inference \
+--config-file /home/kkacanja/ecc_pe/gw200105/teob/vary_l1/22/config.ini \
+--nprocesses 64 \
+--processing-scheme mkl \
+--output-file /home/kkacanja/ecc_pe/gw200105/teob/vary_l1/22/result.hdf \
+--seed 190814 \
+--force \
+--verbose 
