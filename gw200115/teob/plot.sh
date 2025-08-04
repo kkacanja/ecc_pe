91416cb0028c91105297a9dafeb8ac65f07e9d07
kkacanja <your.email@example.com>
2025-08-04 12:27:20 -0400

    adding gw200115

--- gw200115/teob/plot.sh
+++
@@ -1,5 +0,0 @@
-pycbc_inference_plot_posterior --input-file ./result.hdf \
---output-file /home/kkacanja/public_html/ecc_pe/plots/gw200115/gw200115_posteriors_teob.png \
---parameters inclination mchirp q spin1z spin2z eccentricity anomaly \
---z-arg snr \
-#--vmin 10.0 --vmax 10.4 \
