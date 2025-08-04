1a6958a86fd46440adfe41d84e7edf3d32a9b0dc
kkacanja <your.email@example.com>
2025-08-04 12:34:49 -0400

    adding GW230529

--- gw230529/teobHM/plot.sh
+++
@@ -1,4 +0,0 @@
-pycbc_inference_plot_posterior --input-file ./result.hdf \
---output-file /home/kkacanja/public_html/ecc_pe/gw230529_posteriors_teobHM.png \
---parameters inclination mchirp q spin1z spin2z eccentricity anomaly \
---z-arg snr --vmin 11.3 --vmax 11.7 \
